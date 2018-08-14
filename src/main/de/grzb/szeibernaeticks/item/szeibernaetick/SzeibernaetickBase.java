0ad68fd63a4fcb74d62bac79932e492c63433b94
Rattle22 <RasRas2@googlemail.com>
2018-08-14 18:17:47 +0200

    Something something I just wanted to stash

--- src/main/de/grzb/szeibernaeticks/item/szeibernaetick/SzeibernaetickBase.java
+++ src/main/de/grzb/szeibernaeticks/item/szeibernaetick/SzeibernaetickBase.java
@@ -4,11 +4,9 @@ import main.de.grzb.szeibernaeticks.control.Log;
 import main.de.grzb.szeibernaeticks.control.LogType;
 import main.de.grzb.szeibernaeticks.item.ItemBase;
 import main.de.grzb.szeibernaeticks.szeibernaeticks.BodyPart;
-import main.de.grzb.szeibernaeticks.szeibernaeticks.SzeibernaetickCapabilityProvider;
-import main.de.grzb.szeibernaeticks.szeibernaeticks.ISzeibernaetick;
-import main.de.grzb.szeibernaeticks.szeibernaeticks.SzeibernaetickMapper;
 import main.de.grzb.szeibernaeticks.szeibernaeticks.armoury.ArmouryProvider;
 import main.de.grzb.szeibernaeticks.szeibernaeticks.armoury.IArmoury;
+import main.de.grzb.szeibernaeticks.szeibernaeticks.control.Switch;
 import main.de.grzb.szeibernaeticks.szeibernaeticks.event.ISzeibernaetickEventHandler;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -19,36 +17,46 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 /**
- * Denotes an Item usable as an Szeibernaetick. The only method is to identify
- * in what {@code BodyPart} this is to be installed.<br>
+ * TODO: Overhaul this Class Comment Stores and retrieves the data behind a
+ * Szeibernaetick.
+ *
+ *
+ * Structure of an NBTTag containing a Szeibernaeticks Tag: { OtherTag : Value,
+ * "Szeibernaeticks" : { Unlocalized Name : { "Properties" : { "damage" : The
+ * damage value, Item Specific Properties : Their value } } }, AnotherTag :
+ * Value }
+ *
+ * When passing this between methods/objects, the "Szeibernaeticks" Compound Tag
+ * is the base tag: { Unlocalized Name : { "Properties" : { "damage" : The
+ * damage value, Item Specific Properties : Their value } } }
+ *
+ * Naming convention: Compound Tags are written in PascalCase : "Properties",
+ * "ThisIsAdvancedStuff" Primitive Tags are written in camelCase : "damage",
+ * "advancedValue"
+ *
+ *
+ * Denotes an Item usable as an Szeibernaetick.<br>
  * <br>
  * To add a new Szeibernaetick, you need a) an Item implementing
- * ISzeibernaetick, b) any Method of adding it to the
+ * SzeibernaetickBase, b) any Method of adding it to the
  * ISzeibernaetickStorageCapability of an Entity and c) an
  * ISzeibernaetickEventHandler implementing the functionality via
  * EventSubscriptions.
  *
  * @author DemRat
  */
-public class SzeibernaetickBase extends ItemBase {
+public abstract class SzeibernaetickBase extends ItemBase {
 
-    private Class<? extends ISzeibernaetick> capabilityClass;
-
-    public SzeibernaetickBase(String name, Class<? extends ISzeibernaetick> capability,
-            Class<? extends ISzeibernaetickEventHandler> handler) {
-        this(name, capability, handler, CreativeTabs.COMBAT);
+    public SzeibernaetickBase(String name, Class<? extends ISzeibernaetickEventHandler> handler) {
+        this(name, handler, CreativeTabs.COMBAT);
     }
 
-    public SzeibernaetickBase(String name, Class<? extends ISzeibernaetick> capability,
-            Class<? extends ISzeibernaetickEventHandler> handler, CreativeTabs tab) {
+    public SzeibernaetickBase(String name, Class<? extends ISzeibernaetickEventHandler> handler, CreativeTabs tab) {
         super(name);
         Log.log("Creating Item of type: " + this.getClass(), LogType.DEBUG, LogType.INSTANTIATION, LogType.ITEM);
 
-        this.capabilityClass = capability;
-        SzeibernaetickMapper.instance.register(this.capabilityClass, this);
         try {
             MinecraftForge.EVENT_BUS.register(handler.newInstance());
         }
@@ -76,7 +84,7 @@ public class SzeibernaetickBase extends ItemBase {
             IArmoury szeiberArm = playerIn.getCapability(ArmouryProvider.ARMOURY_CAP, null);
             Log.log("Armoury is: " + szeiberArm, LogType.ITEM, LogType.SZEIBER_ARM, LogType.DEBUG, LogType.SPECIFIC);
             // Add the ItemStack to it.
-            if(szeiberArm != null && szeiberArm.addSzeibernaetick(this.getCapabilityInstance())) {
+            if(szeiberArm != null && szeiberArm.addSzeibernaetick(this)) {
                 Log.log("Successfully added Capability! Shrinking stack now.", LogType.ITEM, LogType.DEBUG,
                         LogType.SPECIFIC);
                 thisStack.shrink(1);
@@ -87,41 +95,40 @@ public class SzeibernaetickBase extends ItemBase {
     }
 
     /**
-     * Returns a new instance of the ISzeibernaetick corresponding to this
-     * ISzeibernaetick.
+     * The {@code BodyPart} this Capability inhabits when installed.
      *
-     * @return A new instance of the correct ISzeibernaetick.
+     * @return
      */
-    public ISzeibernaetick getCapabilityInstance() {
-        Log.log("Attempting to instantiate " + this.getClass() + "'s Capability.", LogType.ITEM, LogType.SZEIBER_CAP,
-                LogType.DEBUG);
-        try {
-            ISzeibernaetick capability = this.capabilityClass.newInstance();
-            Log.log("Successfully instantiated capability: " + capability, LogType.ITEM, LogType.SZEIBER_CAP,
-                    LogType.DEBUG);
-            return capability;
-        }
-        catch(InstantiationException e) {
-            Log.log("Cannot instantiate SzeiberCapability from Class Object!", LogType.ERROR);
-            Log.logThrowable(e);
-            e.printStackTrace();
-        }
-        catch(IllegalAccessException e) {
-            Log.log("Cannot access the default constructor of SzeiberCapability!", LogType.ERROR);
-            Log.logThrowable(e);
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public abstract BodyPart getBodyPart();
 
-    public BodyPart getBodyPart() {
-        Log.log("Getting BodyPart.", LogType.ITEM, LogType.SPECIFIC, LogType.DEBUG);
-        return this.getCapabilityInstance().getBodyPart();
-    }
+    /**
+     * Returns an unique Identifier for this capability _class_, not instance.
+     * This is primarily used as part of the key when attaching this capability
+     * to items.
+     *
+     * @return An unique Identifier.
+     */
+    public abstract String getIdentifier();
 
-    @Override
-    public ICapabilityProvider initCapabilities(ItemStack stack, NBTTagCompound nbt) {
-        Log.log("Adding Capability to this item.", LogType.ITEM, LogType.SZEIBER_CAP, LogType.DEBUG);
-        return new SzeibernaetickCapabilityProvider(this.getCapabilityInstance());
-    }
+    /**
+     * Stores this Capability as a NBTTagCompound.
+     *
+     * @return The NBT storing this capability.
+     */
+    public abstract NBTTagCompound toNBT();
+
+    /**
+     * Restores this capabilities values from an NBTTagCompound.
+     *
+     * @param nbt
+     */
+    public abstract void fromNBT(NBTTagCompound nbt);
+
+    /**
+     * Returns a list of switchables which together control all ad-hoc
+     * modifiable state of this Szeibernaetick.
+     * 
+     * @return
+     */
+    public abstract Iterable<Switch> GetSwitches();
 }
